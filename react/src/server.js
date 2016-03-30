78ffb2c6b06423934761533a5d5d821150133eac
Ben Craig <benjamin.d.craig@gmail.com>
2016-03-29 20:18:57 -0400

    updated server from tutorial

--- react/src/server.js
+++ react/src/server.js
@@ -2,17 +2,57 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+
 const app = express();
 
-function start() {
+const COMMENTS_FILE = path.join(__dirname, '../comments.json');
 
-    app.set('port', (process.env.PORT || 8080));
-    app.use(express.static(__dirname + '/public'));
-    app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-    app.listen(app.get('port'), () => {
-        console.log('Server running on localhost:' + app.get('port') + '/');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+app.get('/api/comments', (req, res) => {
+    fs.readFile(COMMENTS_FILE, (err, data) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        res.json(JSON.parse(data));
     });
-}
+});
 
-start();
+app.post('/api/comments', (req, res) => {
+    fs.readFile(COMMENTS_FILE, (err, data) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        
+        var comments = JSON.parse(data);
+
+        var newComment = {
+            id: Date.now(),
+            author: req.body.author,
+            text: req.body.text,
+        };
+        comments.push(newComment);
+        fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            res.json(comments);
+        });
+    });
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Server running on localhost:' + app.get('port') + '/');
+});
