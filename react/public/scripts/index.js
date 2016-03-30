0e1c07644426819c852def20d92a4c4ad1789086
Ben Craig <benjamin.d.craig@gmail.com>
2016-03-29 20:59:10 -0400

    nesting react components

--- react/public/scripts/index.js
+++ react/public/scripts/index.js
@@ -4,11 +4,38 @@ var CommentBox = React.createClass({displayName: 'CommentBox',
         	React.createElement('div', {
        		 		className: "commentBox"
                	},
-          		"Hello, world! I am a CommentBox."
+          		"Hello, world! I am a CommentBox.",
+          		React.createElement(CommentList),
+          		React.createElement(CommentForm)
         	)
         );
     }
 });
+
+var CommentList = React.createClass({displayName: 'CommentList',
+	render: function() {
+		return (
+			React.createElement('div', {
+					className: 'commentList'
+				},
+				"This is a comment list"
+			)
+		);
+	}
+});
+
+var CommentForm = React.createClass({displayName: 'CommentForm',
+	render: function() {
+		return (
+			React.createElement('div', {
+					className: 'commentForm'
+				},
+				"This is a comment form"
+			)
+		);
+	}
+});
+
 ReactDOM.render(
   React.createElement(CommentBox, null),
   document.getElementById('content')
